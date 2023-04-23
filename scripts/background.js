f2594684e0fc2317571fee241f17e9b9669ded93
patakk <paolo.ceric@gmail.com>
2023-04-23 21:42:14 +0200

    init

--- scripts/background.js
+++ scripts/background.js
@@ -1,14 +1,14 @@
 function updateIcon(isEnabled) {
     const path = isEnabled
       ? {
-          "16": "icon16.png",
-          "48": "icon48.png",
-          "128": "icon128.png",
+          "16": "assets/icon16.png",
+          "48": "assets/icon48.png",
+          "128": "assets/icon128.png",
         }
       : {
-          "16": "icon_gray16.png",
-          "48": "icon_gray48.png",
-          "128": "icon_gray128.png",
+          "16": "assets/icon_gray16.png",
+          "48": "assets/icon_gray48.png",
+          "128": "assets/icon_gray128.png",
         };
   
     chrome.action.setIcon({ path: path });
