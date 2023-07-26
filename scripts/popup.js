e8f800cc99d7e316edb0fed084e6149345243263
Raphaël de Courville <groupes.raphael@gmail.com>
2023-07-26 14:01:25 +0200

    Attempt to update the UI (buggy)

--- scripts/popup.js
+++ scripts/popup.js
@@ -47,34 +47,75 @@ async function toggle(key){
     });
 }
 
-// the order of these selectors must match the order of the keys in targetSelectors dict from content.js
-const targetSelectors = [
-    'toggleSignup', 
-    'toggleVerif', 
-    'toggleCheck',
-];
-    
 document.addEventListener("DOMContentLoaded", () => {
-    for(const key of targetSelectors){
-        document.getElementById(key).addEventListener("click", () => {toggle(key)});
-    }
-
+    // Gets the isEnabled dict from storage
     chrome.storage.sync.get(["isEnabled"], (result) => {
-        if(result.isEnabled !== undefined){
-            let num = 0;
-            // iterate through isEnabled dict
-            for(let key of targetSelectors){
-                if(result.isEnabled[key]) num++;
+        const isEnabledDict = result.isEnabled || {}; // Default to an empty object if not set
+        
+        // Iterate through targets dict
+        for(const target in targets) {
+            const targetElement = document.getElementById(target);
+
+            // Initialize the display state
+            if (isEnabledDict[target]) {
+                targetElement.textContent = "visible"; // or whatever representation you use for "enabled"
+            } else {
+                targetElement.textContent = "hidden"; // or whatever representation you use for "disabled"
             }
-            updateIcon(num > 0);
-            updateToggle(result.isEnabled);
-        }
-        else{
-            updateIcon(true);
-            let flagBySelector = {};
-            for(const key of targetSelectors) flagBySelector[key] = true;
-            updateToggle(flagBySelector);
-            chrome.storage.sync.set({ isEnabled: flagBySelector });
+
+            // Add event listener for toggling
+            targetElement.addEventListener("click", () => {
+                // Toggle state
+                isEnabledDict[target] = !isEnabledDict[target];
+                
+                // Update display state
+                if (isEnabledDict[target]) {
+                    targetElement.textContent = "visible";
+                } else {
+                    targetElement.textContent = "hidden";
+                }
+
+                // Save the updated isEnabled dictionary back to storage
+                chrome.storage.sync.set({ "isEnabled": isEnabledDict });
+            });
         }
     });
 });
+
+
+function generatePopupContent() {
+    const mainDiv = document.createElement('div');
+    mainDiv.id = "main";
+
+    // Adding the header
+    const header = document.createElement('h2');
+    header.textContent = 'BlueBeGone';
+    mainDiv.appendChild(header);
+
+    const description = document.createElement('p');
+    description.id = 'description';
+    description.textContent = 'Hides some of Twitter\'s annoyances.';
+    mainDiv.appendChild(description);
+
+    for (const key in targets) {
+        const rowDiv = document.createElement('div');
+        rowDiv.className = 'row';
+
+        const titleDiv = document.createElement('div');
+        titleDiv.textContent = targets[key].title;
+
+        const toggleDiv = document.createElement('div');
+        toggleDiv.className = 'toggle';
+        toggleDiv.id = key;
+        toggleDiv.textContent = 'hidden';
+
+        rowDiv.appendChild(titleDiv);
+        rowDiv.appendChild(toggleDiv);
+
+        mainDiv.appendChild(rowDiv);
+    }
+
+    document.body.appendChild(mainDiv);
+}
+
+generatePopupContent();
