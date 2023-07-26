bcc8b8d613c53174c5085510f0136892d3db20eb
Raphaël de Courville <groupes.raphael@gmail.com>
2023-07-26 15:05:08 +0200

    Moved target selectors to target.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,3 @@
-// Targets
-const targets = {
-    'toggleSignup': {
-        'selector': 'a[href="/i/verified-choose"][role="link"]',
-        'title': 'Hide Signup',
-        'description': 'Hide the signup button for Twitter verification',
-    },
-    'toggleCheck': {
-        'selector': 'svg[data-testid="icon-verified"]',
-        'title': 'Hide Checkmarks',
-        'description': 'Hide the checkmarks for verified Twitter accounts',
-    },
-};
-
 // Extract the selectors from the targets
 const targetSelectors = Object.values(targets).map(target => target.selector);
 
